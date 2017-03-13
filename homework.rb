c08659db30338829831a6a48c806720d0d3ad8db
Rhys Brame <rhysbrame@gmail.com>
2017-03-13 21:45:27 +0000

    upload homework

---
+++ homework.rb
@@ -0,0 +1,95 @@
+class Student
+
+  def initialize(student_name,cohort,prog_language)
+    @student_name = student_name
+    @cohort = cohort
+    @prog_language = prog_language
+  end
+
+  def student_name
+    return @student_name
+  end
+
+  def cohort
+    return @cohort
+  end
+
+  def set_student_name(new_name)
+    @student_name = new_name
+  end
+
+  def set_cohort(new_cohort)
+    @cohort = new_cohort
+  end
+
+  def talks(words)
+    return words
+  end
+
+  def talks_language()
+    return "I love " + @prog_language
+  end
+end
+
+class Team
+
+  attr_reader :team_name
+  attr_accessor :players, :coach, :points
+
+  def initialize(team_name, players_array, coach, points)
+    @team_name = team_name
+    @players = players_array
+    @coach = coach
+    @points = points
+
+  end
+
+  def add_player(new_player)
+    @players << new_player
+  end
+
+  def player_search(searched)
+    @players.include?(searched)
+  end
+
+  def win_or_lose(result)
+    if result == "W"
+      @points += 3
+    elsif result == "D"
+      @points += 1
+    elsif result == "L"
+      return
+    end
+
+  end
+
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ homework_spec.rb
@@ -0,0 +1,100 @@
+require ('minitest/autorun')
+require_relative ('homework.rb')
+require('minitest/rg')
+
+class TestStudent < MiniTest::Test
+
+  def setup
+    @student = Student.new("Rhys",11,"ruby")
+  end
+
+  def test_student_name
+    assert_equal("Rhys",@student.student_name)
+  end
+
+  def test_cohort
+    assert_equal(11,@student.cohort)
+  end
+
+  def test_set_student_name
+    @student.set_student_name("Darren")
+    assert_equal("Darren", @student.student_name)
+  end
+
+  def test_set_cohort
+    @student.set_cohort(13)
+    assert_equal(13,@student.cohort)
+  end
+
+  def test_talks
+    talking = @student.talks("Something")
+    assert_equal("Something",talking)
+  end
+
+  def test_talks_language
+    talking = @student.talks_language()
+    assert_equal("I love ruby",talking)
+  end
+
+end
+
+class TestTeam < MiniTest::Test
+
+  def setup
+    players_array = ["Chris Cousins","Jordan Reed", "DeShaun Jackson"]
+    @team = Team.new("Washington Redskins", players_array, "Jay Gruden", 0)
+  end
+
+  def test_team_name
+    assert_equal("Washington Redskins", @team.team_name)
+  end
+
+  def test_team_coach
+    assert_equal("Jay Gruden", @team.coach)
+  end
+
+  def test_players_array
+    assert_equal(  ["Chris Cousins","Jordan Reed", "DeShaun Jackson"], @team.players)
+  end
+
+  def test_set_coach
+    @team.coach = "Jon Gruden"
+    assert_equal("Jon Gruden", @team.coach)
+  end
+
+  def test_add_player
+    @team.add_player("Bennie Logan")
+    assert_equal(4, (@team.players).length)
+  end
+
+  def test_player_search
+    assert_equal(true, @team.player_search("DeShaun Jackson"))
+  end
+
+  def test_team_points
+    assert_equal(0,@team.points)
+  end
+
+  def test_win_or_lose
+    @team.win_or_lose("D")
+    assert_equal(1, @team.points)
+  end
+
+end
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
